04ec244fd038cb7790e2872c20bdf0c97ee19c0d
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-07-21 02:04:09 +0900

    feat: remove preview label

--- web/src/components/layout/Header/Header.tsx
+++ web/src/components/layout/Header/Header.tsx
@@ -8,12 +8,7 @@ export const Header: FC = () => {
   return (
     <div>
       <div className="fixed left-0 top-0 z-10 flex h-10 w-full items-center gap-4 bg-pink px-3 text-white shadow-2xl">
-        <div className="grow truncate font-bold">
-          Kheopswap{" "}
-          <span className="align-top text-xs font-normal text-primary-150">
-            preview
-          </span>
-        </div>
+        <div className="grow truncate font-bold">Kheopswap</div>
         <RelaySelect />
         <ConnectButton />
       </div>
