ad94337fc7ee09add6316beb16a181872f9ebde9
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-08-03 14:39:40 +0900

    chore bump deps and add biome vite plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import biomePlugin from "vite-plugin-biome";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [react(), svgr(), biomePlugin()],
 	resolve: {
 		alias: {
 			src: "/src",
