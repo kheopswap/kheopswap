cc6f67f8b48f5cd6b26cef3c7d008a1dc673e455
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-11-16 20:59:52 +0900

    fix: shimmer

--- web/src/components/Shimmer.tsx
+++ web/src/components/Shimmer.tsx
@@ -11,8 +11,9 @@ export const Shimmer: FC<{ children?: ReactNode; className?: string }> = ({
 		<Pulse
 			pulse
 			className={cn(
-				"select-none rounded bg-neutral-800 text-neutral-800",
+				"select-none rounded",
 				className,
+				"bg-neutral-800 text-neutral-800",
 			)}
 		>
 			{children}
