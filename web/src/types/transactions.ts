3179d12481b5397e4df34f087638622ca71b0a76
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-03-13 09:37:22 +0900

    feat: papi v2 rc4

--- web/src/types/transactions.ts
+++ web/src/types/transactions.ts
@@ -1,4 +1,4 @@
 import type { Transaction } from "polkadot-api";
 
 // biome-ignore lint/suspicious/noExplicitAny: this is the way
-export type AnyTransaction = Transaction<any, any, any, any>;
+export type AnyTransaction = Transaction<any, any>;
