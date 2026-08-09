cb876aa52dca9bdda7e42fdf6ab8fefddfec766c
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-08-09 18:17:13 +0900

    🔥 remove unused icon exports and mapper re-export

--- web/src/registry/tokens/mappers/index.ts
+++ web/src/registry/tokens/mappers/index.ts
@@ -1,5 +1,4 @@
 export { createSufficientMap } from "./createSufficientMap.ts";
-export { isEthereumOriginLocation } from "./isEthereumOriginLocation.ts";
 export { mapAssetTokensFromEntries } from "./mapAssetTokensFromEntries.ts";
 export { mapForeignAssetTokensFromEntries } from "./mapForeignAssetTokensFromEntries.ts";
 export { mapPoolAssetTokensFromEntries } from "./mapPoolAssetTokensFromEntries.ts";
